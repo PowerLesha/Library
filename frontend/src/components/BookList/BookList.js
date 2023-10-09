51167dca9707c334044b41442f7d839eb0eb8b1d
Lesha <leshapowerlifter@gmail.com>
2023-10-09 22:54:04 +0300

    added filter

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -3,9 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
 import { BsBookmarkStarFill, BsBookmarkStar } from "react-icons/bs";
 import "./BookList.css";
+import { selectTitleFilter } from "../../redux/slices/filterSlice";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const titleFilter = useSelector(selectTitleFilter);
   const dispatch = useDispatch();
   const handledeleteBook = (id) => {
     dispatch(deleteBook(id));
@@ -13,6 +15,13 @@ const BookList = () => {
   const handleToggleFavorite = (id) => {
     dispatch(toggleFavorite(id));
   };
+
+  const filteredBooks = books.filter((book) => {
+    const matchesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase());
+    return matchesTitle;
+  });
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -20,7 +29,7 @@ const BookList = () => {
         <p>No books available</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Filter.css";
+import {
+  selectTitleFilter,
+  setTitleFilter,
+} from "../../redux/slices/filterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitleFilter);
+
+  const handleTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+  };
   return (
     <div className="app-block filter">
-      <h2>Filters</h2>
+      <div className="filter-group">
+        <input
+          value={titleFilter}
+          type="text"
+          placeholder="Filter by title..."
+          onChange={handleTitleFilterChange}
+        />
+      </div>
     </div>
   );
 };
